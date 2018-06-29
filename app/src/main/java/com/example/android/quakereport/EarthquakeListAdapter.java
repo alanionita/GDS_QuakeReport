a5fe545ec4c967a4831d40ee6d198e35a9fb1072
Alan Ionita <a.g.ionita@gmail.com>
2018-06-29 03:34:13 +0100

    feat(JSONparsin): parses the JSON correctly, and interprets the UNIX time as date, and time

--- app/src/main/java/com/example/android/quakereport/EarthquakeListAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeListAdapter.java
@@ -9,7 +9,9 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by alanionita on 28/06/2018.
@@ -37,13 +39,36 @@ public class EarthquakeListAdapter extends ArrayAdapter<Earthquake> {
         TextView magnitude = (TextView) convertView.findViewById(R.id.magnitude);
         TextView location = (TextView) convertView.findViewById(R.id.location);
         TextView date = (TextView) convertView.findViewById(R.id.date);
+        TextView time = (TextView) convertView.findViewById(R.id.time);
 
         if (currentEarthquake != null) {
             magnitude.setText(currentEarthquake.getMagnitude().toString());
             location.setText(currentEarthquake.getLocation());
-            date.setText(currentEarthquake.getDate().toString());
+
+
+            Date dateObject = new Date(currentEarthquake.getTimeInMili());
+            String formatedDate = formatDate(dateObject);
+            date.setText(formatedDate);
+            String formatedTime = formatTime(dateObject);
+            time.setText(formatedTime);
         }
 
         return convertView;
+    }
+
+    /**
+     * Return the formatted date string (i.e. "Mar 3, 1984") from a Date object.
+     */
+    private String formatDate(Date dateObject) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd LLL, yyyy");
+        return dateFormat.format(dateObject);
+    }
+
+    /**
+     * Return the formatted date string (i.e. "4:30 PM") from a Date object.
+     */
+    private String formatTime(Date dateObject) {
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm a");
+        return timeFormat.format(dateObject);
     }
 }
